31afefac2ab7f70b521bec99d379be945951f2db
matvejvasiljev <matvej.vasiljev.2010@icloud.com>
2023-04-14 14:58:16 +0300

    finished css and added a menu for opening images fullscreen

---
+++ db.js
@@ -0,0 +1,50 @@
+let db = `
+0.jpg
+---
+The Menin Road
+---
+Nash received the commission for this work, which was orginally to have been called 'A Flanders
+Battlefield', from the Ministry of Information in April 1918. It was to feature in a Hall of
+Remembrance devoted to "fighting subjects, home subjects and the war at sea and in the air".
+---
+https://artsandculture.google.com/partner/imperial-war-museums
+@@@
+1.jpg
+---
+Children's Games
+---
+Pieter Bruegel the Elder
+---
+From a bird’s-eye view – the only way Bruegel could legibly fit in the impressive number of figures –
+the viewer looks down onto a wide square with a transition from an urban to a rural setting at the
+edges. On the right the view opens on to a long street laid out in central perspective and leading
+to the city centre, where a church steeple (or town-hall tower) soars into the sky.
+---
+https://artsandculture.google.com/partner/kunsthistorisches-museum-vienna-museum-of-fine-arts
+@@@
+2.jpg
+---
+Landscape with an Aqueduct
+---
+Théodore Géricault
+---
+his work is one of a projected set of four monumental landscapes representing the times of day that
+Gericault painted in his Paris studio. (Only three were completed.) Conceived as a decorative
+ensemble, the paintings fuse souvenirs of ruins in the Italian countryside, which the artist had
+visited in 1816 and 1817, with the grand manner exemplified by past masters Nicolas Poussin and
+Joseph Vernet.
+---
+https://artsandculture.google.com/partner/the-metropolitan-museum-of-art
+@@@
+3.jpg
+---
+The Adoration of the Shepherds
+---
+Giorgione
+---
+Giorgione portrayed the main scene on the right, in front of a dark grotto, while on the left is
+bright landscape crowned by trees. A sincere dramatic tension is obtained by the choice to place the
+kneeling shepherd pilgrims in the centre of the painting. The entire group of parents, child, and pilgrim form an anchored rectangle that forms a counterpoised focal point to the receding landscape on the left.
+---
+https://artsandculture.google.com/partner/imperial-war-museums
+`
---
+++ script.js
@@ -0,0 +1,10 @@
+let modal = document.getElementsByClassName("modal")[0]
+
+function openImage(image) {
+    // console.log("image " + image +" opened");
+    modal.style.display = "block"
+}
+
+function closeImage(image) {
+    modal.style.display = "none"
+}
